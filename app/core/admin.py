00394960c3f70da8f90f6eab5609d605e477f630
findmebytola <91450266+findmebytola@users.noreply.github.com>
2021-10-23 00:15:55 +0700

    update

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Indicator
 
-# Register your models here.
+
+@admin.register(Indicator)
+class IndicatorAdmin(admin.ModelAdmin):
+    list_display = ("scorecard_name", "step", "milestone",
+                    "score", "narrative", "year")
+    list_filter = ("scorecard_name", "year")
+    search_fields = ["scorecard_name"]
--- app/core/models.py
+++ app/core/models.py
@@ -3,3 +3,21 @@ from django.db import models
 
 class BookInfo(models.Model):
     title = models.CharField(max_length=255)
+
+
+class Indicator(models.Model):
+
+    def __str__(self) -> str:
+        return self.scorecard_name
+
+    SCORE = [
+        (0, "Not yet achieved"),
+        (1, "Patially achieved"),
+        (2, "Fully achieved"),
+    ]
+    scorecard_name = models.CharField(max_length=255)
+    step = models.IntegerField()
+    milestone = models.CharField(max_length=255)
+    score = models.IntegerField(choices=SCORE)
+    narrative = models.TextField(max_length=700)
+    year = models.IntegerField()
